91de1e1b7c3247734c927ca1fd06567cb5f607a5
MonyJR <mony@MacBook-Air.local>
2024-07-09 10:34:09 +0700

    fix port

--- Tsoa with Express/src/server.ts
+++ Tsoa with Express/src/server.ts
@@ -1,11 +1,11 @@
-import configs from './utils/config';
+// import configs from './utils/config';
 import app from './app';
 import connectDB from './database/connection';
 
 function running(){
   connectDB()
-  app.listen(configs.port, () => {
-  console.log(`Server is running on port ${configs.port}`);
+  app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
 });
 }
 
