bc32245fb4bc5b83c376ccd2f9c397a514b3eb98
Mony-JR <bronyfb2000@gmail.com>
2024-07-11 14:02:22 +0700

    @3.0.1

--- Tsoa with Express/src/server.ts
+++ Tsoa with Express/src/server.ts
@@ -7,7 +7,7 @@ import connectDB from './database/connection';
 function running(){
   connectDB()
   app.listen(configs.port, () => {
-  console.log(`Server is running on port 3000`);
+  console.log(`Server is running on port ${configs.port}`);
 });
 }
 
